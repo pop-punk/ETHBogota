61d2c3e74dcad28878ed55c07460279b6f49a04a
leggio <harrison@legg.io>
2022-10-08 09:22:10 -0500

    first commit

---
+++ tailwind-landing-page-template-main/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import { Web3Modal } from '@web3modal/react';
+
+import "aos/dist/aos.css";
+import "./css/style.css";
+
+import AOS from "aos";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
+
+const config = {
+  projectId: '402c23e1dcd93e001de7bf2cc30343d4',
+  theme: 'dark',
+  accentColor: 'default',
+  ethereum: {
+    appName: 'web3Modal'
+  }
+}
+
+
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+      <Web3Modal config={config} />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind-landing-page-template-main/src/pages/SignUp.jsx
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import Header from "../partials/Header";
+
+import { ConnectButton, useAccount } from "@web3modal/react";
+import { ethers } from "ethers";
+
+import { Web3Storage, File } from "web3.storage";
+
+function SignUp() {
+  const [threshold, setThreshold] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  let navigate = useNavigate();
+
+  const { address, connector, isConnected } = useAccount();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Store addresses + threshold
+    const obj = { owner: address, threshold: threshold, addresses: [address1, address2] };
+    const buffer = Buffer.from(JSON.stringify(obj));
+
+    const files = [
+      new File([buffer], `${address}.json`),
+    ];
+
+    console.log(files);
+
+    const client = new Web3Storage({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEZCNjE5MjJkYzk4QTI2QUMzQjJEOTQyYTMxNDNlMzRjNzYxMERkOTgiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyMTA0ODY5MjAsIm5hbWUiOiJTaWdjdXJlIn0.Mxck2M7MQP26yEXo4oZrWfznxk_O_BiVLpw2llqLGM4",
+    });
+
+    const cid = await client.put(files)
+    console.log('stored files with cid:', cid)
+
+    console.log(address);
+
+    // Store passwords
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+              {/* Page header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h1 className="h2">Secure password management.</h1>
+                <h1 className="h2">Protected by multisig technology.</h1>
+              </div>
+
+              {/* Form */}
+              <div className="max-w-sm mx-auto">
+                <form>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    {!isConnected && (
+                      <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                        <ConnectButton />
+                      </div>
+                    )}
+                    {isConnected && (
+                      <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                        <p>Connected as {address}</p>
+                      </div>
+                    )}
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="threshold"
+                      >
+                        Signature Threshold{" "}
+                        <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="threshold"
+                        type="number"
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter the signature threshold"
+                        required
+                        onInput={(e) => setThreshold(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="address1"
+                      >
+                        Address #1 <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="address1"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter the first address"
+                        required
+                        onInput={(e) => setAddress1(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="address2"
+                      >
+                        Address #2 <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="address2"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter the second address"
+                        required
+                        onInput={(e) => setAddress2(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="password1"
+                      >
+                        Password #1 <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="password1"
+                        type="password"
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter the first password"
+                        required
+                        onInput={(e) => setPassword1(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="password2"
+                      >
+                        Password #2 <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="password2"
+                        type="password"
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter the second password"
+                        required
+                        onInput={(e) => setPassword2(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button
+                        className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                        onClick={handleSubmit}
+                      >
+                        Sign up
+                      </button>
+                    </div>
+                  </div>
+                </form>
+                <div className="flex items-center my-6">
+                  <div
+                    className="border-t border-gray-300 flex-grow mr-3"
+                    aria-hidden="true"
+                  ></div>
+                  <div className="text-gray-600 italic">Or</div>
+                  <div
+                    className="border-t border-gray-300 flex-grow ml-3"
+                    aria-hidden="true"
+                  ></div>
+                </div>
+                <form></form>
+                <div className="text-gray-600 text-center mt-6">
+                  Already using Sigcure?{" "}
+                  <Link
+                    to="/signin"
+                    className="text-blue-600 hover:underline transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default SignUp;
