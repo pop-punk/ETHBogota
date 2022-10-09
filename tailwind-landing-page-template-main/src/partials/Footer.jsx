2ffe96cd92e06b48cfd58def34f94bd8f96300af
Erin Engelmann <erinengelmann@Erins-MacBook-Air.local>
2022-10-09 01:57:51 -0500

    soontm

--- tailwind-landing-page-template-main/src/partials/Footer.jsx
+++ tailwind-landing-page-template-main/src/partials/Footer.jsx
@@ -6,13 +6,6 @@ function Footer() {
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
-        {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-
-          
-          {/* 5th block */}
-          
-        </div>
 
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
